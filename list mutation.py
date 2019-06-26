b136df63980bed72df8c2ea8acf9eb57947839ff
alice1981 <alicemai1981@gmail.com>
2019-06-26 22:25:38 +1000

    obj += obj2 is different with obj = obj+obj2 when obj and obj2 are mutable

---
+++ list mutation.py
@@ -0,0 +1,15 @@
+#use += operator to add element to list
+alist =['a', 'b', 'c', 'd']
+print(id(alist))
+alist +=['e']
+print(alist)
+print(id(alist))
+
+# use + to add element to list
+alist =['a', 'b', 'c', 'd']
+alist = alist +['e']
+print(alist)
+print(id(alist))
+
+
+
