1a47d2a51ffd3af6dc36813df9806a0643c1c0b8
alice1981 <alicemai1981@gmail.com>
2019-06-29 22:08:38 +1000

    add 5 to list items

---
+++ add_value.py
@@ -0,0 +1,12 @@
+# add 5 to each item in numbs and numbs 2
+
+numbs = [5,4,6,7,10]
+numbs = [i+5 for i in numbs]
+print(numbs)
+
+
+numbs2 = [1,2,3,4,5]
+
+for i in numbs2:
+    numbs2 = i+5
+    print(numbs2)
